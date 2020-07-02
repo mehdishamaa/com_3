2d6e095d166124e9fbaab67b6d2315516180c050
Mehdi Shamaa <mshamaa@spartaglobal.com>
2020-07-02 10:51:21 +0100

    Functions Notes

--- Week 3 - Python/Functions.py
+++ Week 3 - Python/Functions.py
@@ -6,5 +6,30 @@
 # It saves us from repeating ourselves, re-usuability of code
 
 # How do we create a function?
-# Syntax:
+# Create a function to greet:
+
+# def greeting():
+#    return "Hello World!"
+# pass is used to skip the function
+# return is used to provide an output
+
+# However, in the above code, there is no output because the function has not been called.
+# How do we call the function?
+
+# print(greeting())
+
+# Methods with arguments:
+
+def add_values(number1, number2):
+    return number1 + number2    # We can return anything - string - integer
+
+print(add_values(4,7))
+
+# Above, we have created a function that will add any two values.
+# The function called add_values allows us to specify two inegers which it will add together when we asl to print the function.
+
+
+
+
+
 
