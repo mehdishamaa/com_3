92824683ef1d7306f5c07088f78da55f9d84c887
Mehdi Shamaa <mshamaa@spartaglobal.com>
2020-07-02 10:26:57 +0100

    Functions Notes

---
+++ Week 3 - Python/Functions.py
@@ -0,0 +1,10 @@
+# What is a function?
+# A function is a re-usuable block of code
+
+
+# Why should we use a function?
+# It saves us from repeating ourselves, re-usuability of code
+
+# How do we create a function?
+# Syntax:
+
