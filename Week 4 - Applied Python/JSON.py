3f72606d3a29f85e2233135752f8230a51a7c65c
Mehdi Shamaa <mshamaa@spartaglobal.com>
2020-07-07 10:16:33 +0100

    JSON Notes

---
+++ Week 4 - Applied Python/JSON.py
@@ -0,0 +1,13 @@
+# What is JSON?
+# Javascript Object Notation
+
+# Syntax: JSON is a syntax for exchanging data
+# Where and how?: Between a browser and server
+# What type of data?: The data can only be text
+# How is data stored?: Data is stored using key value pairs
+
+import json
+
+# encoding from dictionary and writing to jsonfile
+car_data = {"name": "tesla", "engine":"electric"} #dictionary
+print(car_data)
