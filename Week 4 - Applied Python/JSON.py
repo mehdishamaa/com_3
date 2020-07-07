c12c98ca2b9ceb6aecd581989b2553994f9b35a3
Mehdi Shamaa <mshamaa@spartaglobal.com>
2020-07-07 10:53:12 +0100

    JSON Notes

--- Week 4 - Applied Python/JSON.py
+++ Week 4 - Applied Python/JSON.py
@@ -11,3 +11,33 @@ import json
 # encoding from dictionary and writing to jsonfile
 car_data = {"name": "tesla", "engine":"electric"} #dictionary
 print(car_data)
+
+# checking type of dictionary
+
+print(type(car_data))
+
+# Let us now change Python dictionary to JSON string
+
+car_data_json_string = json.dumps(car_data)
+
+print(type(car_data_json_string))
+
+# Let's create a JSON file with writing permission
+
+
+
+with open("new_json_file.json", "w") as jsonfile:
+    json.dump(car_data, jsonfile)  # json.dump takes two arguments
+    # "name of file", "permission type"
+    # The first is the dictionary, the second is file_type on this occasion jsonfile
+
+    # Reading from the file we just created
+
+with open("new_json_file.json") as jsonfile: # Decoding
+    car = json.load(jsonfile) # Storing data from file to car variable
+    print(type(car))
+
+
+
+
+
